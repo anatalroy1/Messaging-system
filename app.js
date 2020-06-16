68a22e862b20c42db7877a1664eb9d346d649af8
Anat Alroy <anatalroy@gmail.com>
2020-06-16 14:54:44 +0300

    add query parser

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require('cors');
+const queryParser = require('express-query-parser');
 const userRoutes = require('./src/routes/userRoutes');
 const messagesRoutes = require('./src/routes/messagesRoutes');
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(queryParser({ parseNull: true, parseBoolean: true}));
 
 app.use('/user', userRoutes);
 app.use('/messages', messagesRoutes);
